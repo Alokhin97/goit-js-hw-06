4a81db6eeedd1ac23056ecd3205e0af6c54a4b0c
Anton Alokhin <anton.alyokhin1997@gmail.com>
2023-02-24 22:02:45 +0200

    24.02.2023

--- js/task-01.js
+++ js/task-01.js
@@ -1,12 +1,12 @@
 const body = document.body;
 const childrenBody = body.children;
 console.log(childrenBody);
-childrenBody.forEach((array) => {
+childrenBody.forEach((element, index, array) => {
   const list = array[1];
 });
 console.log(list);
 const listItems = list.children;
 console.log(listItems);
-listItems.forEach((array) => {
+listItems.forEach((element, index, array) => {
   console.log(`Number of categories: ${array.length}`);
 });
